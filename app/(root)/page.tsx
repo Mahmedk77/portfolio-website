7a394bc7116d323e79ce3b33131cb026d3586080
Mahmedk77 <mak.212601@gmail.com>
2025-11-29 14:12:53 +0500

    create portfolio repo

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Home Page
+      <h1>Users:</h1>
+      <li><Link href={'/users/1'}> User 1 </Link> </li>
+      <li><Link href={'/users/2'}> User 2 </Link> </li>
+      <li><Link href={'/users/3'}> User 3 </Link> </li>
+    </div>
+  )
+}
+
+export default page
